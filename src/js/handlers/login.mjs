8c61eab9b4439e65f5130c459aff84714f9568c6
bmsf <fromreide94@gmail.com>
2022-11-24 10:22:46 +0100

    login handler

---
+++ src/js/handlers/login.mjs
@@ -0,0 +1,25 @@
+import { login } from '../api/auth/login.mjs';
+/**
+ * Retrieves value from form when user creates new account
+ * and sends it to the api
+ * @example
+ * ```js
+ * // Get data from form and send it to api
+ * const formValues = gets values that user inputs
+ * const formData = uses FormData class to collect key/value pairs of values from input provided by user
+ *
+ * ```
+ */
+export const loginFormListener = () => {
+	const form = document.querySelector('#loginForm');
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		const formValues = event.target;
+		const formData = new FormData(formValues);
+
+		const newProfile = Object.fromEntries(formData.entries());
+
+		login(newProfile);
+	});
+};
