7768505030e075ca5dc169add46b8ee269564c9e
bmsf <fromreide94@gmail.com>
2022-12-07 17:16:54 +0100

    added getposts and getpost

--- src/js/api/posts/get.mjs
+++ src/js/api/posts/get.mjs
@@ -1,3 +1,23 @@
-export const getPosts = async () => {};
+import { tokenFetch } from '../tokenFetch.mjs';
+import { API_SOCIAL_URL } from '../constants.mjs';
 
-export const getPost = async (id) => {};
+const action = '/posts';
+
+export const getPosts = async () => {
+	const getPostURL = `${API_SOCIAL_URL}${action}`;
+
+	const response = await tokenFetch(getPostURL);
+
+	return await response.json();
+};
+
+export const getPost = async (id) => {
+	if (!id) {
+		throw new Error('Delete requires a postID');
+	}
+	const getPostURL = `${API_SOCIAL_URL}${action}/${id}`;
+
+	const response = await tokenFetch(getPostURL);
+
+	return await response.json();
+};
