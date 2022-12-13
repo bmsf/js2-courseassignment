7554ca63471ba146c306766c2f8bff659e6aaa9d
bmsf <fromreide94@gmail.com>
2022-12-13 12:34:03 +0100

    jsdocs for create

--- src/js/api/posts/create.mjs
+++ src/js/api/posts/create.mjs
@@ -4,6 +4,14 @@ import { API_SOCIAL_URL } from '../constants.mjs';
 const action = '/posts';
 const method = 'post';
 
+/**
+ *
+ * @param {promise} postData
+ * @returns {array}
+ * Function which uses authorization token to make a user eligible for
+ * creating a post using dedicated function tokenFetch()
+ */
+
 export const createPost = async (postData) => {
 	const createPostURL = API_SOCIAL_URL + action;
 	const body = JSON.stringify(postData);
